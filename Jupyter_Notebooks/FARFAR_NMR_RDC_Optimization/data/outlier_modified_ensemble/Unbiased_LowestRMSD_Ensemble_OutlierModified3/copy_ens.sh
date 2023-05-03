b93b3cfdcf24f338033b056710a78ddce5cfce35
Rohit Roy <al.hashimi.lab@gmail.com>
2023-05-02 21:08:49 -0400

    test

--- Jupyter_Notebooks/FARFAR_NMR_RDC_Optimization/data/outlier_modified_ensemble/Unbiased_LowestRMSD_Ensemble_OutlierModified3/copy_ens.sh
+++
@@ -1,9 +0,0 @@
-fromdir="/mnt/d4_new/rohit/ENS/wtTAR/Unbiased_LowestRMSD_Ensemble/validation/pdb/E0"
-
-for i in {1..18}
-do
-    cp $fromdir/ens-${i}.pales.pdb pdb/E0
-done
-
-mv ens-19.pdb pdb/E0/ens-19.pales.pdb
-cp $fromdir/ens-20.pales.pdb pdb/E0
